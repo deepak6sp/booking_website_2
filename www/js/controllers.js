010c60cf32d2cc55ee4a2a4e5065163c55f603a9
deepak6sp <deepak6sp@gmail.com>
2015-12-21 22:29:57 +1100

    http access added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -72,15 +72,12 @@ angular.module('starter.controllers', [])
  };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('PlaylistsCtrl', function($scope,$http) {
+  $http.get("http://localhost/bookmovie/bookmovie/public/hello")
+  .then(function(response){
+    $scope.playlists = response.data;
+    console.log(response.data);
+  });
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
